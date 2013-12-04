e9aa4a61f98d5190bb4a29daeaefe0b5a9190101
rickihastings <rickihastings22@gmail.com>
2013-12-04 11:55:40 +0000

    Moved the tests to irc.js

--- test/irc.js
+++ test/irc.js
@@ -17,8 +17,15 @@ var network = Object.freeze({
 describe('IRC Events', function () {
 	it('registered event should have nick property', function (done) {
 		var socket = new Client('key', network, MockGenericSocket);
-		Events.on('key.registered', function(o) {
+		Events.once('key.registered', function(o) {
 			o.should.have.property('nick');
+			done();
+		});
+	});
+
+	it('registered event nick property should equal testbot', function (done) {
+		var socket = new Client('key', network, MockGenericSocket);
+		Events.once('key.registered', function(o) {
 			o.nick.should.equal('testbot');
 			done();
 		});
